088fed69f979bd3801f5b1645337ba4454f2f60d
Rohan Mehra <48274899+RohanMehra@users.noreply.github.com>
2026-04-16 00:49:26 +0530

    mcp client  code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 MCP Code practice
+Created an AI agent that can use external tools from 2 servers:
+
+math → local MCP server (stdio)
+weather → HTTP MCP server (streaming)
--- client.py
+++ client.py
@@ -1,47 +1,54 @@
 from langchain_mcp_adapters.client import MultiServerMCPClient
-from langgraph.prebuilt import create_react_agent
+from langchain.agents import create_agent  
 from langchain_groq import ChatGroq
 
 from dotenv import load_dotenv
 load_dotenv()
 
 import asyncio
+import os
+
 
 async def main():
-    client=MultiServerMCPClient(
+    client = MultiServerMCPClient(
         {
-            "math":{
-                "command":"python",
-                "args":["mathserver.py"], ## Ensure correct absolute path
-                "transport":"stdio",
-            
+            "mathServer": {
+                "command": "python",
+                "args": ["mathServer.py"],
+                "transport": "stdio",
             },
-            "weather": {
-                "url": "http://localhost:8000/mcp",  # Ensure server is running here
-                "transport": "streamable_http",
+            "weatherServer": {
+                "url": "http://localhost:8000/mcp",
+                "transport": "streamable-http",  
             }
-
         }
     )
 
-    import os
-    os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+    os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
-    tools=await client.get_tools()
-    model=ChatGroq(model="qwen-qwq-32b")
-    agent=create_react_agent(
-        model,tools
+    tools = await client.get_tools()
+
+   
+    model = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
+
+    agent = create_agent(
+        model=model,
+        tools=tools
     )
 
+ 
     math_response = await agent.ainvoke(
-        {"messages": [{"role": "user", "content": "what's (3 + 5) x 12?"}]}
+        {"messages": [{"role": "user", "content": "what is 3 + 5?"}]}
     )
 
-    print("Math response:", math_response['messages'][-1].content)
+    print("Math response:", math_response["messages"][-1].content)
 
     weather_response = await agent.ainvoke(
         {"messages": [{"role": "user", "content": "what is the weather in California?"}]}
     )
-    print("Weather response:", weather_response['messages'][-1].content)
 
-asyncio.run(main())
+    print("Weather response:", weather_response["messages"][-1].content)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
